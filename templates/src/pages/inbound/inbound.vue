562b06238ec39c71b28a72a4e205e940cfb5d54f
singosgu <singosgu@gmail.com>
2020-12-06 09:59:28 +0800

    V2.0.0 Beta3

--- templates/src/pages/inbound/inbound.vue
+++ templates/src/pages/inbound/inbound.vue
@@ -6,14 +6,30 @@
       <q-tabs
         v-model="detaillink"
       >
-  <q-route-tab name="asn" label="ASN" icon="img:statics/inbound/asn.png" to="/inbound/asn" exact/>
-  <q-route-tab name="predeliverystock" label="Pre Delivery" icon="img:statics/inbound/polist.png" to="/inbound/predeliverystock" exact/>
-  <q-route-tab name="preloadstock" label="Pre Load" icon="img:statics/inbound/preloadstock.png" to="/inbound/preloadstock" exact/>
-  <q-route-tab name="presortstock" label="Sorting" icon="img:statics/inbound/presortstock.png" to="/inbound/presortstock" exact/>
-  <q-route-tab name="sortstock" label="Sorted" icon="img:statics/inbound/sortstock.png" to="/inbound/sortstock" exact/>
-  <q-route-tab name="shortage" label="Shortage Qty" icon="img:statics/inbound/shortage.png" to="/inbound/shortage" exact/>
-  <q-route-tab name="more" label="More Qty" icon="img:statics/inbound/more.png" to="/inbound/more" exact/>
-  <q-route-tab name="asnfinish" label="Receiving List" icon="img:statics/inbound/asnfinish.png" to="/inbound/asnfinish" exact/>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="asn" label="ASN" icon="img:statics/inbound/asn.png" to="/inbound/asn" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="predeliverystock" label="Pre Delivery" icon="img:statics/inbound/polist.png" to="/inbound/predeliverystock" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="preloadstock" label="Pre Load" icon="img:statics/inbound/preloadstock.png" to="/inbound/preloadstock" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="presortstock" label="Sorting" icon="img:statics/inbound/presortstock.png" to="/inbound/presortstock" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="sortstock" label="Sorted" icon="img:statics/inbound/sortstock.png" to="/inbound/sortstock" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="shortage" label="Shortage Qty" icon="img:statics/inbound/shortage.png" to="/inbound/shortage" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="more" label="More Qty" icon="img:statics/inbound/more.png" to="/inbound/more" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="asnfinish" label="Receiving List" icon="img:statics/inbound/asnfinish.png" to="/inbound/asnfinish" exact/>
+        </transition>
       </q-tabs>
     </div>
   </div>
