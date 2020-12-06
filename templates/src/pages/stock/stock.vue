46db9e4941fab2b9bac30110cfe82559a5b0f898
singosgu <singosgu@gmail.com>
2020-12-06 11:37:54 +0800

    V2.0.0 Beta3

---
+++ templates/src/pages/stock/stock.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-top">
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 100%">
+      <q-tabs
+        v-model="detaillink"
+      >
+  <q-route-tab name="stocklist" label="Stock List" icon="img:statics/stock/stocklist.png" to="/stock/stocklist" exact/>
+  <q-route-tab name="stockbinlist" label="Bin Stock" icon="img:statics/warehouse/binset.png" to="/stock/stockbinlist" exact/>
+  <q-route-tab name="emptybin" label="Empty Bin" icon="img:statics/stock/cyclecount.png" to="/stock/emptybin" exact/>
+  <q-route-tab name="occupiedbin" label="Occupied Bin" icon="img:statics/stock/cyclecount.png" to="/stock/occupiedbin" exact/>
+      </q-tabs>
+    </div>
+  </div>
+</template>
+    <router-view />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Pagestock',
+  data () {
+    return {
+      detaillink: 'stock'
+    }
+  },
+  methods: {
+  }
+}
+</script>
