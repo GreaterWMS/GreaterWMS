8c778389872d0de898bd08ae43fcfe601f8d0acf
singosgu <singosgu@gmail.com>
2020-12-06 10:54:15 +0800

    V2.0.0 Beta3
    Former-commit-id: a252582429dd3cb929ac2d04e46d829dec2164a5 [formerly 4feb2d707438f14fcd5b1bdebe33cbfbbba0aceb] [formerly 5586691126eef11cbc1658968f41ce4e4e07204b [formerly 790fc9279e0fb0a13ab2e30670a63e8ca33d7ad0 [formerly e41708fc654468622a8feebb14a95746da0527bd] [formerly 68123bb8dd7f097d90cfde958699b1e3fb1e9cc7] [formerly 68123bb8dd7f097d90cfde958699b1e3fb1e9cc7 [formerly 7c51a0f14bf56d3cf3c7e89c3c650d377e8c396c]]]]
    Former-commit-id: a498fb013f3b66da5858f211fcc0f9d8d3543cad [formerly 8c1244a1de5aa523056f5833d87c1a6340170658]
    Former-commit-id: 64b57c7744e50af1f67aede8d7bbc95c920d0da4
    Former-commit-id: 032d89f3431778af1374d38fcfb16f10509c03e1
    Former-commit-id: 0a5cbb1944d8d6cb9108187961c3273fd486600d [formerly 10f67342b129db1d5b7bc26f3c4244897d962553]
    Former-commit-id: 5c3732109699e57e6c7f4de5246b2011d5897674

---
+++ templates/src/pages/staff/staff.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-top">
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 100%">
+      <q-tabs
+        v-model="detaillink"
+      >
+  <q-route-tab name="stafflist" label="Staff" icon="img:statics/staff/stafflist.png" to="/staff/stafflist" exact/>
+      </q-tabs>
+    </div>
+  </div>
+</template>
+    <router-view />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Pagestaff',
+  data () {
+    return {
+      detaillink: 'staff'
+    }
+  },
+  methods: {
+  }
+}
+</script>
