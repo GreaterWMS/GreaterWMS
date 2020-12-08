7e15f7fb39977cde56da7fb0e5b9fbf97cc281ab
singosgu <singosgu@gmail.com>
2020-12-08 11:25:33 +0800

    V2.0.0 Beta3

--- django_wms/__init__.py
+++ django_wms/__init__.py
@@ -1,6 +1,17 @@
-import mimetypes
+import mimetypes, os, requests, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_wms.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
+from django.conf import settings
 
 mimetypes.add_type("text/css", ".css", True)
 mimetypes.add_type("text/javascript", ".js", True)
-
+try:
+    response = requests.post('https://www.56yhz.com/area/')
+    path = os.path.join(settings.BASE_DIR, 'utils/country.txt')
+    with open(path, 'w') as f:
+        f.write(str(eval(response.text).get('country')))
+        f.close()
+except:
+    pass
 print('Welcome To GreaterWMS')
