8dbb07cb2899cf2f79a66badc8d1f7d2ed6c1098
singosgu <singosgu@gmail.com>
2020-12-08 08:56:31 +0800

    V2.0.0 Beta3
    Former-commit-id: eaaee50a8fc35ecb6ec91052600faf6614d5fb77 [formerly 8f042b6853e465c146827ac04b2d10503e8002f3] [formerly f8236ccd4bb80c93e9146c40183bd16a908ab8d0 [formerly 7e6cae31ccc288b56cb5588ba598f26cf84ac7c5 [formerly cd9277be7657924751767d2c2999e30818212a69] [formerly 337c6c532cf8880867105759df25556ce9303b6c] [formerly 337c6c532cf8880867105759df25556ce9303b6c [formerly 09850515c2ecdd9501fe468cc90b41dd977c44bd]]]]
    Former-commit-id: 0600f44afe1bdddbe229266c51ef29f5f46205ef [formerly dd1c76ae1be7e062a646eada5958fd43d3967f7b]
    Former-commit-id: 38a90249882514c1201f84801854ee54a3c5e6c6
    Former-commit-id: 435fe5412dbec91a471b3e928188e2f25d6d7d7f
    Former-commit-id: dea5dca41f2c1db73170c1e2aa23a5ab28b0f196 [formerly dbd47ec5b8efd95b62807edb712c4c276f411698]
    Former-commit-id: bfebf5cbef7193363e60693a9c49ebaa39692144

---
+++ templates/src/pages/stock/stock.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-page class="flex flex-top">
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 100%">
+      <q-tabs
+        v-model="detaillink"
+      >
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="stocklist" label="Stock List" icon="img:statics/stock/stocklist.png" to="/stock/stocklist" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="stockbinlist" label="Bin Stock" icon="img:statics/warehouse/binset.png" to="/stock/stockbinlist" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="emptybin" label="Empty Bin" icon="img:statics/stock/cyclecount.png" to="/stock/emptybin" exact/>
+        </transition>
+        <transition appear enter-active-class="animated zoomIn">
+          <q-route-tab name="occupiedbin" label="Occupied Bin" icon="img:statics/stock/cyclecount.png" to="/stock/occupiedbin" exact/>
+        </transition>
+      </q-tabs>
+    </div>
+  </div>
+</template>
+    <router-view />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Pagestock',
+  data () {
+    return {
+      detaillink: 'stock'
+    }
+  },
+  methods: {
+  }
+}
+</script>
