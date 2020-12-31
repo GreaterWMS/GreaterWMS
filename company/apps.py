10991a239ca208d123f77bc682e45b175dc1a7b1
singosgu <singosgu@gmail.com>
2020-12-31 12:54:12 +0800

    update
    Former-commit-id: c40667fb15c999c82d2386ec5fdbece7a0b93191 [formerly 408003655ae95be987288786b711921b9c75b7b2] [formerly c3a35b921b68a66bed49c2ea6b3c2a0bd68effdf [formerly 5d7ffe582395d8d3e7d490270e6f40d908c40201 [formerly 5d7ffe582395d8d3e7d490270e6f40d908c40201 [formerly a5a7926b73b3399e890746f4d87f887d7b4db047]]]]
    Former-commit-id: 8e1069b0094dfbfd440756be75df7127f01de3d6 [formerly 61f0477c2598e1c89489eaf1a7f2692fa456a630]
    Former-commit-id: fc6caca68e11af13031e685e692731c1b1a2f82f
    Former-commit-id: 1b5648f754a5bd5ea5370fd651c19571549acb09
    Former-commit-id: 21fbed8bffac571ddf2e45bc8e4707a58a74e7fe

--- company/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CompanyConfig(AppConfig):
-    name = 'company'
--- inbound/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InboundConfig(AppConfig):
-    name = 'inbound'
