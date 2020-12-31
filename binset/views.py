aea67f73d69d18adda208d7a6e01a6e6d7a78a67
singosgu <singosgu@gmail.com>
2020-12-31 12:58:20 +0800

    V2.0.0 Beta4
    Former-commit-id: b451c47342e325d060e27fdb6f9a99843ddfc99d [formerly 9ebc4f1d7182ecf19727c431ccc19c3f7a91a169] [formerly 12648ceb7e98a8ed7cce4dcca064d4c95d58e2d1 [formerly 4bc79e46395b0a5e6327ceee3cf031839c653022 [formerly 4bc79e46395b0a5e6327ceee3cf031839c653022 [formerly c543edfc5e4d314250b99db7c6dd098a1a1fb192]]]]
    Former-commit-id: cf48d1abda7507f224a9cd1de90ee6d409957366 [formerly 93fedcd98828633ce0d1460f1484f7f098a482ce]
    Former-commit-id: 0486ab693467b47af4209c5ee4c5f4a9546c36c0
    Former-commit-id: 56a0b220c7ff9d5cd5249774fe81ad9320f0d00e
    Former-commit-id: 5716d03cb76aeecfaf25dde87f5350424c187c46

---
+++ binset/views.py
@@ -0,0 +1,141 @@
+from rest_framework import viewsets
+from .models import ListModel
+from . import serializers
+from .page import MyPageNumberPagination
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from .filter import Filter
+from rest_framework.exceptions import APIException
+from django.db.models import Q
+from binsize.models import ListModel as binsize
+from binproperty.models import ListModel as binproperty
+from warehouse.models import ListModel as warehouse
+
+class APIViewSet(viewsets.ModelViewSet):
+    """
+        retrieve:
+            Response a data list（get）
+
+        list:
+            Response a data list（all）
+
+        create:
+            Create a data line（post）
+
+        delete:
+            Delete a data line（delete)
+
+        partial_update:
+            Partial_update a data（patch：partial_update）
+
+        update:
+            Update a data（put：update）
+    """
+    queryset = ListModel.objects.all()
+    serializer_class = serializers.BinsetGetSerializer
+    pagination_class = MyPageNumberPagination
+    filter_backends = [DjangoFilterBackend, OrderingFilter, ]
+    ordering_fields = ['id', "create_time", "update_time", ]
+    filter_class = Filter
+
+    def get_project(self):
+        try:
+            id = self.kwargs.get('pk')
+            return id
+        except:
+            return None
+
+    def get_queryset(self):
+        id = self.get_project()
+        if self.request.user:
+            if id is None:
+                return self.queryset.filter(openid=self.request.auth.openid, is_delete=False)
+            else:
+                return self.queryset.filter(openid=self.request.auth.openid, id=id, is_delete=False)
+        else:
+            return self.queryset.none()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.BinsetGetSerializer
+        elif self.action == 'retrieve':
+            return serializers.BinsetGetSerializer
+        elif self.action == 'create':
+            return serializers.BinsetPostSerializer
+        elif self.action == 'update':
+            return serializers.BinsetUpdateSerializer
+        elif self.action == 'partial_update':
+            return serializers.BinsetPartialUpdateSerializer
+        elif self.action == 'destroy':
+            return serializers.BinsetGetSerializer
+        else:
+            return self.http_method_not_allowed(request=self.request)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data['openid'] = request.auth.openid
+        if self.queryset.filter(openid=data['openid'], bin_name=data['bin_name'], is_delete=False).exists():
+            raise APIException({"detail": "Data exists"})
+        else:
+            if binsize.objects.filter(openid=data['openid'], bin_size=data['bin_size'], is_delete=False).exists():
+                if binproperty.objects.filter(Q(openid=data['openid'], bin_property=data['bin_property'], is_delete=False) |
+                                              Q(openid='init_data', bin_property=data['bin_property'], is_delete=False)).exists():
+                    serializer = self.get_serializer(data=data)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save()
+                    headers = self.get_success_headers(serializer.data)
+                    return Response(serializer.data, status=200, headers=headers)
+                else:
+                    raise APIException({"detail": "Bin property does not exists or it has been changed"})
+            else:
+                raise APIException({"detail": "Bin size does not exists or it has been changed"})
+
+    def update(self, request, pk):
+        qs = self.get_object()
+        if qs.openid != self.request.auth.openid:
+            raise APIException({"detail": "Cannot update data which not yours"})
+        else:
+            data = request.data
+            if binsize.objects.filter(openid=self.request.auth.openid, bin_size=data['bin_size'], is_delete=False).exists():
+                if binproperty.objects.filter(Q(openid=self.request.auth.openid, bin_property=data['bin_property'], is_delete=False) |
+                                              Q(openid='init_data', bin_property=data['bin_property'], is_delete=False)).exists():
+                    serializer = self.get_serializer(qs, data=data)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save()
+                    headers = self.get_success_headers(serializer.data)
+                    return Response(serializer.data, status=200, headers=headers)
+                else:
+                    raise APIException({"detail": "Bin property does not exists or it has been changed"})
+            else:
+                raise APIException({"detail": "Bin size does not exists or it has been changed"})
+
+    def partial_update(self, request, pk):
+        qs = self.get_object()
+        if qs.openid != self.request.auth.openid:
+            raise APIException({"detail": "Cannot partial_update data which not yours"})
+        else:
+            data = request.data
+            if binsize.objects.filter(openid=self.request.auth.openid, bin_size=data['bin_size'], is_delete=False).exists():
+                if binproperty.objects.filter(Q(openid=self.request.auth.openid, bin_property=data['bin_property'], is_delete=False) |
+                                              Q(openid='init_data', bin_property=data['bin_property'], is_delete=False)).exists():
+                    serializer = self.get_serializer(qs, data=data, partial=True)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save()
+                    headers = self.get_success_headers(serializer.data)
+                    return Response(serializer.data, status=200, headers=headers)
+                else:
+                    raise APIException({"detail": "Bin property does not exists or it has been changed"})
+            else:
+                raise APIException({"detail": "Bin size does not exists or it has been changed"})
+
+    def destroy(self, request, pk):
+        qs = self.get_object()
+        if qs.openid != self.request.auth.openid:
+            raise APIException({"detail": "Cannot delete data which not yours"})
+        else:
+            qs.is_delete = True
+            qs.save()
+            serializer = self.get_serializer(qs, many=False)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=200, headers=headers)
