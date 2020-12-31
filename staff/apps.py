ca5927f354c5ea8b5175b3da77dd67e0aba1183e
singosgu <singosgu@gmail.com>
2020-12-31 12:54:12 +0800

    update

--- staff/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StaffConfig(AppConfig):
-    name = 'staff'
--- utils/country.py
+++
@@ -1,5 +0,0 @@
-
-def Country_Data():
-    with open('.country.txt', 'r') as f:
-        data = f.read()
-        return data
