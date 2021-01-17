f066f70672fbb9dadb448969386f9e1f0721ed92
singosgu <singosgu@gmail.com>
2021-01-17 15:56:01 +0800

    V2.0.0 Beta4
    Former-commit-id: cb2f3436068ba65e58ab224624f6ac9c4cef2529 [formerly f3b2bbdbb94a7019bdcbf2eb0eb6b518ea8fd2b6] [formerly 0927992e6d5b26459f252071e412a241cdabb13b [formerly 9a595a1ae43334997f1a238f0e860cc5ce767d1b [formerly 9a595a1ae43334997f1a238f0e860cc5ce767d1b [formerly 6d936eff3962aa2d4caffc2838c8ec1062fed191]]]]
    Former-commit-id: cd1fb89ac9884fce4d38df91672c1c2fd7d69ed4 [formerly cddba6698e07bb09349caacb06e536f13c87a554]
    Former-commit-id: b752d30440d31b616b7b9b3d68116c1ab0c58395
    Former-commit-id: 6087fd895efa0539f8ceb9ab83a0a5ae3de7e620
    Former-commit-id: f9fcab7276aff9a15fe4c5de0cf2a7316b1d3ddc

--- asn/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import AsnListModel, AsnDetailModel
-
-admin.site.register(AsnListModel)
-admin.site.register(AsnDetailModel)
--- userprofile/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserprofileConfig(AppConfig):
-    name = 'userprofile'
