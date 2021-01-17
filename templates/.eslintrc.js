0cb7578f18367280566cb1418350154d0b503796
singosgu <singosgu@gmail.com>
2021-01-17 16:08:33 +0800

    V2.0.0 Beta5
    *Fix Bug and add all data download to csv
    
    
    Former-commit-id: 6f14a73ef7a2da2e5b6fbebca98d5c1fe1322e3c [formerly 682c77765862340621057213e4288f7c128fe4b9] [formerly 464d424fc1ca951fc00691c03acf7b62da2d662a [formerly 53ef34eb5af1d792bde2eff41eb8fc6a94e88fe5 [formerly 53ef34eb5af1d792bde2eff41eb8fc6a94e88fe5 [formerly 2302aa4d14146f5044461744fe1d93b7658b675d]]]]
    Former-commit-id: 3e307c89468c02f02b7ea8be4ee334bfa78ca0f6 [formerly 7d9b426efa59ff9e838e7d4a9ee34b3dbdce7b5a]
    Former-commit-id: 73c3f771fdadee012c56737ed7223c399f7779d7
    Former-commit-id: e05005c67f2730254673f78f4daa0101fe7e102a
    Former-commit-id: 83128fcba7b50442bdf1f49feac486df99469478

---
+++ templates/.eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+  // This option interrupts the configuration hierarchy at this file
+  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+  root: true,
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
+  },
+
+  env: {
+    browser: true
+  },
+
+  // Rules order is important, please avoid shuffling them
+  extends: [
+    // Base ESLint recommended rules
+    // 'eslint:recommended',
+
+
+    // Uncomment any of the lines below to choose desired strictness,
+    // but leave only one uncommented!
+    // See https://eslint.vuejs.org/rules/#available-rules
+    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
+    // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
+    // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+
+    'standard'
+
+  ],
+
+  plugins: [
+    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
+    // required to lint *.vue files
+    'vue',
+
+  ],
+
+  globals: {
+    ga: true, // Google Analytics
+    cordova: true,
+    __statics: true,
+    process: true,
+    Capacitor: true,
+    chrome: true
+  },
+
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow paren-less arrow functions
+    'arrow-parens': 'off',
+    'one-var': 'off',
+
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-promise-reject-errors': 'off',
+
+
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
