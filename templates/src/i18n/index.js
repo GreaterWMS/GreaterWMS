8bf12051d928f84d49e94580249a1ddf21920bb9
singosgu <singosgu@gmail.com>
2021-02-05 13:32:32 +0800

    V2.0.0 Beta5

---
+++ templates/src/i18n/index.js
@@ -0,0 +1,11 @@
+import enUS from './en-us'
+import zhHans from './zh-hans'
+import zhHant from './zh-hant'
+import Ja from './ja'
+
+export default {
+  'en-us': enUS,
+  'zh-hans': zhHans,
+  'zh-hant': zhHant,
+  ja: Ja
+}
