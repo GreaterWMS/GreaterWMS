b8182e39b2d692936ad81ffd14d5a3830c505731
singosgu <singosgu@gmail.com>
2021-02-21 23:15:08 +0800

    delete

--- dn/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import DnListModel, DnDetailModel
-
-admin.site.register(DnListModel)
-admin.site.register(DnDetailModel)
--- stock/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import StockListModel, StockBinModel
-
-admin.site.register(StockListModel)
-admin.site.register(StockBinModel)
