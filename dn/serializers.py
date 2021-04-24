dcc14763c58dec954c8d73ba27eff34722d0b95e
Singosgu <Singosgu@gmail.com>
2021-04-24 22:05:12 +0800

    V 2.0.18

--- dn/serializers.py
+++ dn/serializers.py
@@ -1,152 +1,169 @@
 from rest_framework import serializers
-from .models import AsnListModel, AsnDetailModel
+from .models import DnListModel, DnDetailModel, PickingListModel
 from utils import datasolve
 
-class ASNListGetSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=True, required=False)
-    asn_status = serializers.IntegerField(read_only=True, required=False)
-    supplier = serializers.CharField(read_only=True, required=False)
+class DNListGetSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=True, required=False)
+    dn_status = serializers.IntegerField(read_only=True, required=False)
+    customer = serializers.CharField(read_only=True, required=False)
     creater = serializers.CharField(read_only=True, required=False)
     create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
     update_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
     class Meta:
-        model = AsnListModel
+        model = DnListModel
         exclude = ['openid', 'is_delete', ]
-        read_only_fields = ['id', 'openid', ]
+        read_only_fields = ['id', ]
 
-class ASNListPostSerializer(serializers.ModelSerializer):
+class DNListPostSerializer(serializers.ModelSerializer):
     openid = serializers.CharField(read_only=False, required=False, validators=[datasolve.openid_validate])
-    asn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.asn_data_validate])
-    supplier = serializers.CharField(read_only=False, required=False)
+    dn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.dn_data_validate])
+    customer = serializers.CharField(read_only=False, required=False)
     creater = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
     class Meta:
-        model = AsnListModel
+        model = DnListModel
         exclude = ['is_delete', ]
         read_only_fields = ['id', 'create_time', 'update_time', ]
 
-class ASNListPartialUpdateSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.asn_data_validate])
+class DNListPartialUpdateSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.dn_data_validate])
 
     class Meta:
-        model = AsnListModel
+        model = DnListModel
         exclude = ['is_delete', ]
         read_only_fields = ['id', 'create_time', 'update_time', ]
 
-class ASNListUpdateSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.asn_data_validate])
+class DNListUpdateSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.dn_data_validate])
 
     class Meta:
-        model = AsnListModel
+        model = DnListModel
         exclude = ['is_delete', ]
         read_only_fields = ['id', 'create_time', 'update_time', ]
 
-class ASNDetailGetSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=True, required=False)
-    supplier = serializers.CharField(read_only=True, required=False)
+class DNDetailGetSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=True, required=False)
+    dn_status = serializers.IntegerField(read_only=True, required=False)
+    customer = serializers.CharField(read_only=True, required=False)
     goods_code = serializers.CharField(read_only=True, required=False)
     goods_qty = serializers.IntegerField(read_only=True, required=False)
-    goods_actual_qty = serializers.IntegerField(read_only=True, required=False)
-    sorted_qty = serializers.IntegerField(read_only=True, required=False)
-    goods_shortage_qty = serializers.IntegerField(read_only=True, required=False)
-    goods_more_qty = serializers.IntegerField(read_only=True, required=False)
-    goods_damage_qty = serializers.IntegerField(read_only=True, required=False)
+    pick_qty = serializers.IntegerField(read_only=True, required=False)
+    picked_qty = serializers.IntegerField(read_only=True, required=False)
+    intransit_qty = serializers.IntegerField(read_only=True, required=False)
+    delivery_actual_qty = serializers.IntegerField(read_only=True, required=False)
+    delivery_shortage_qty = serializers.IntegerField(read_only=True, required=False)
+    delivery_more_qty = serializers.IntegerField(read_only=True, required=False)
+    delivery_damage_qty = serializers.IntegerField(read_only=True, required=False)
+    goods_weight = serializers.FloatField(read_only=True, required=False)
+    goods_volume = serializers.FloatField(read_only=True, required=False)
     creater = serializers.CharField(read_only=True, required=False)
     create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
     update_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
+    back_order_label = serializers.BooleanField(read_only=True, required=False)
     class Meta:
-        model = AsnDetailModel
+        model = DnDetailModel
         exclude = ['openid', 'is_delete', ]
         read_only_fields = ['id', 'openid']
 
-class ASNDetailPostSerializer(serializers.ModelSerializer):
+class DNDetailPostSerializer(serializers.ModelSerializer):
     openid = serializers.CharField(read_only=False, required=False, validators=[datasolve.openid_validate])
-    asn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
-    supplier = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
+    dn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
+    customer = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
     goods_code = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
     goods_qty = serializers.IntegerField(read_only=False, required=True, validators=[datasolve.qty_0_data_validate])
     creater = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
     class Meta:
-        model = AsnDetailModel
+        model = DnDetailModel
         exclude = ['is_delete', ]
         read_only_fields = ['id', 'create_time', 'update_time', ]
 
-class ASNSortedPostSerializer(serializers.ModelSerializer):
-    openid = serializers.CharField(read_only=False, required=False, validators=[datasolve.openid_validate])
-    asn_code = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
-    supplier = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
-    goods_code = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
-    goods_qty = serializers.IntegerField(read_only=False, required=True, validators=[datasolve.qty_data_validate])
-    creater = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
-    class Meta:
-        model = AsnDetailModel
-        exclude = ['is_delete', ]
-        read_only_fields = ['id', 'create_time', 'update_time', ]
-
-class ASNDetailUpdateSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
-    supplier = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
+class DNDetailUpdateSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
+    customer = serializers.CharField(read_only=False,  required=True, validators=[datasolve.data_validate])
     goods_code = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
     goods_qty = serializers.IntegerField(read_only=False, required=True, validators=[datasolve.qty_0_data_validate])
     creater = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
     class Meta:
-        model = AsnDetailModel
+        model = DnDetailModel
         exclude = ['openid', 'is_delete', ]
         read_only_fields = ['id', 'create_time', 'update_time', ]
 
-class ASNDetailPartialUpdateSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=False, required=False, validators=[datasolve.data_validate])
-    supplier = serializers.CharField(read_only=False,  required=False, validators=[datasolve.data_validate])
+class DNDetailPartialUpdateSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=False, required=False, validators=[datasolve.data_validate])
+    customer = serializers.CharField(read_only=False,  required=False, validators=[datasolve.data_validate])
     goods_code = serializers.CharField(read_only=False, required=False, validators=[datasolve.data_validate])
     goods_qty = serializers.IntegerField(read_only=False, required=False, validators=[datasolve.qty_0_data_validate])
     creater = serializers.CharField(read_only=False, required=False, validators=[datasolve.data_validate])
     class Meta:
-        model = AsnDetailModel
+        model = DnDetailModel
         exclude = ['openid', 'is_delete', ]
         read_only_fields = ['id', 'create_time', 'update_time', ]
 
-class MoveToBinSerializer(serializers.ModelSerializer):
-    bin_name = serializers.CharField(read_only=False, required=True, validators=[datasolve.data_validate])
-    qty = serializers.IntegerField(read_only=False, required=True, validators=[datasolve.qty_0_data_validate])
+class DNPickingListGetSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=True, required=False)
+    bin_name = serializers.CharField(read_only=True, required=False)
+    goods_code = serializers.CharField(read_only=True, required=False)
+    pick_qty = serializers.IntegerField(read_only=True, required=False)
+    picked_qty = serializers.IntegerField(read_only=True, required=False)
+    creater = serializers.CharField(read_only=True, required=False)
+    t_code = serializers.CharField(read_only=True, required=False)
+    create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
+    update_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
     class Meta:
-        model = AsnDetailModel
-        ref_name = 'AsnMoveToBin'
-        exclude = ['openid', 'is_delete', ]
-        read_only_fields = ['id', 'create_time', 'update_time', ]
+        model = PickingListModel
+        exclude = ['openid', ]
+        read_only_fields = ['id', ]
+
+class DNPickingCheckGetSerializer(serializers.ModelSerializer):
+    dn_code = serializers.CharField(read_only=True, required=False)
+    bin_name = serializers.CharField(read_only=True, required=False)
+    goods_code = serializers.CharField(read_only=True, required=False)
+    pick_qty = serializers.IntegerField(read_only=True, required=False)
+    picked_qty = serializers.IntegerField(read_only=True, required=False)
+    creater = serializers.CharField(read_only=True, required=False)
+    create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
+    update_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
+    class Meta:
+        model = PickingListModel
+        exclude = ['openid', ]
+        read_only_fields = ['id', ]
 
 class FileListRenderSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=False, required=False)
-    asn_status = serializers.IntegerField(read_only=False, required=False)
+    dn_code = serializers.CharField(read_only=False, required=False)
+    dn_status = serializers.IntegerField(read_only=False, required=False)
     total_weight = serializers.FloatField(read_only=False, required=False)
     total_volume = serializers.FloatField(read_only=False, required=False)
-    supplier = serializers.CharField(read_only=False, required=False)
+    customer = serializers.CharField(read_only=False, required=False)
     creater = serializers.CharField(read_only=False, required=False)
-    transportation_fee = serializers.JSONField(read_only=False, required=False)
+    back_order_label = serializers.BooleanField(read_only=False, required=False)
     create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
     update_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
 
     class Meta:
-        model = AsnListModel
-        ref_name = 'ASNFileListRenderSerializer'
+        model = DnListModel
+        ref_name = 'DNFileListRenderSerializer'
         exclude = ['openid', 'is_delete', ]
 
 class FileDetailRenderSerializer(serializers.ModelSerializer):
-    asn_code = serializers.CharField(read_only=False, required=False)
-    asn_status = serializers.IntegerField(read_only=False, required=False)
+    dn_code = serializers.CharField(read_only=False, required=False)
+    dn_status = serializers.IntegerField(read_only=False, required=False)
+    customer = serializers.CharField(read_only=False, required=False)
     goods_code = serializers.CharField(read_only=False, required=False)
     goods_qty = serializers.IntegerField(read_only=False, required=False)
-    goods_actual_qty = serializers.IntegerField(read_only=False, required=False)
-    sorted_qty = serializers.IntegerField(read_only=False, required=False)
-    goods_shortage_qty = serializers.IntegerField(read_only=False, required=False)
-    goods_more_qty = serializers.IntegerField(read_only=False, required=False)
-    goods_damage_qty = serializers.IntegerField(read_only=False, required=False)
+    pick_qty = serializers.IntegerField(read_only=False, required=False)
+    picked_qty = serializers.IntegerField(read_only=False, required=False)
+    intransit_qty = serializers.IntegerField(read_only=False, required=False)
+    delivery_actual_qty = serializers.IntegerField(read_only=False, required=False)
+    delivery_shortage_qty = serializers.IntegerField(read_only=False, required=False)
+    delivery_more_qty = serializers.IntegerField(read_only=False, required=False)
+    delivery_damage_qty = serializers.IntegerField(read_only=False, required=False)
     goods_weight = serializers.FloatField(read_only=False, required=False)
     goods_volume = serializers.FloatField(read_only=False, required=False)
-    supplier = serializers.CharField(read_only=False, required=False)
     creater = serializers.CharField(read_only=False, required=False)
     create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
     update_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
+    back_order_label = serializers.BooleanField(read_only=False, required=False)
 
     class Meta:
-        model = AsnDetailModel
-        ref_name = 'ASNFileDetailRenderSerializer'
+        model = DnDetailModel
+        ref_name = 'DNFileDetailRenderSerializer'
         exclude = ['openid', 'is_delete', ]
