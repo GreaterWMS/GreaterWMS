100fcbe96e84af0c1db91579df1d6821b0f861b5
Singosgu <Singosgu@gmail.com>
2021-04-28 15:57:33 +0800

    V 2.0.19
    Former-commit-id: 749926022b08af8cb3e3e519a30da3118ba0fd02 [formerly 72d6d15eecb08d87669807152abab51033875a68] [formerly bca3023cba6140709cd938813813ec4b10421680 [formerly 6a0f9f7c488f73f6d6619d518858fb0a8dc445bd [formerly e775f75b5f38ccfc449e601334ac10148f1aa4e6] [formerly 94e0d9de8b84f628b7f4b873df524b25915c46de [formerly 41a1a96863b7ada785753bcb7e6aa2d7fa1412ae]] [formerly 41a1a96863b7ada785753bcb7e6aa2d7fa1412ae [formerly 22170be94079ab830497d8fd9c329ba2e7e3d08c]]]]
    Former-commit-id: d589dd22c32e81cdb6b20b0c3aa3a4a9c62dcfea [formerly 25526c6fd193b4370f004f295eff7d3d7c16f15b]
    Former-commit-id: 1bf1bb8e7e48634a204de69576a52204baa1e8b4
    Former-commit-id: f82528f582814caec3e2cdacd1161258b4d9130a
    Former-commit-id: 71531ec29895f494a45716959df8c98502994a4d [formerly 0dc0e469e86c836bd0875ad2bbe3cb8cc59d44dc]
    Former-commit-id: b80db87b5144658ee3a8ace81364e5166c2e5331

--- asn/filter.py
+++ asn/filter.py
@@ -24,9 +24,18 @@ class AsnDetailFilter(FilterSet):
             "id": ['exact', 'gt', 'gte', 'lt', 'lte', 'isnull', 'in', 'range'],
             "asn_code": ['exact', 'iexact', 'contains', 'icontains'],
             "asn_status": ['exact', 'iexact'],
+            "supplier": ['exact', 'iexact', 'contains', 'icontains'],
+            "goods_code": ['exact', 'iexact', 'contains', 'icontains'],
+            "goods_qty": ['exact', 'iexact', 'gt', 'lt', 'gte', 'lte'],
             "goods_actual_qty": ['exact', 'iexact', 'gt', 'lt', 'gte', 'lte'],
+            "sorted_qty": ['exact', 'iexact', 'gt', 'lt', 'gte', 'lte'],
             "goods_shortage_qty": ['exact', 'iexact', 'gt', 'lt', 'gte', 'lte'],
             "goods_more_qty": ['exact', 'iexact', 'gt', 'lt', 'gte', 'lte'],
+            "goods_damage_qty": ['exact', 'iexact', 'gt', 'lt', 'gte', 'lte'],
+            "goods_weight": ['exact', 'iexact', 'gt', 'gte', 'lt', 'lte', 'isnull', 'in', 'range'],
+            "goods_volume": ['exact', 'iexact', 'gt', 'gte', 'lt', 'lte', 'isnull', 'in', 'range'],
+            "creater": ['exact', 'iexact', 'contains', 'icontains'],
+            "is_delete": ['exact', 'iexact'],
             "create_time": ['year', 'month', 'day', 'week_day', 'gt', 'gte', 'lt', 'lte', 'range'],
             "update_time": ['year', 'month', 'day', 'week_day', 'gt', 'gte', 'lt', 'lte', 'range']
         }
--- dn/urls.py
+++ dn/urls.py
@@ -1,23 +1,7 @@
-"""singosgu URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
 
 urlpatterns = [
 path(r'list/', views.DnListViewSet.as_view({"get": "list", "post": "create"}), name="dnlist"),
@@ -56,5 +40,5 @@ re_path(r'^pod/(?P<pk>\d+)/$', views.DnPODViewSet.as_view({
     'post': 'create',
 }), name="pod_1"),
 path(r'filelist/', views.FileListDownloadView.as_view({"get": "list"}), name="dnfilelistdownload"),
-path(r'filedetail/', views.FileDetailDownloadView.as_view({"get": "list"}), name="dnfiledetaildownload"),
+path(r'filedetail/', views.FileDetailDownloadView.as_view({"get": "list"}), name="dnfiledetaildownload")
 ]
