5311a9e4da0b08a3e53b4ec1a685608ff0a1a70a
Singosgu <Singosgu@gmail.com>
2021-04-30 15:17:42 +0800

    V 2.0.19
    Former-commit-id: 8d445d22fe8bcbf3003e121d57f1e6909a479fff [formerly 4213e65be0309bbbc7aea0e13ea14b07a9ceed44] [formerly 2791363da3de9cf84e27ce1d34ce18d1df49093d [formerly 0bf50f5fde91672ed436de2a9d0e31573b39d252 [formerly 9dce7a3ef55c4a7e8e21f8e4a07f14c3fce528ee] [formerly 8914ee0046d17441e9dcb41134192c486ef4e4b2] [formerly 8914ee0046d17441e9dcb41134192c486ef4e4b2 [formerly e336da439a02a90c46fde458d7528a52ec6bdaa0]]]]
    Former-commit-id: 6fb3ab4fdbc756b83857c4fe8fe0bc3aa60b3b55 [formerly 445636ed7fbf8b69dadf2ada739f956d8d53d1a6]
    Former-commit-id: f9ec51ed19940fe81a433604028a696437941e36
    Former-commit-id: ed2ce8fcf0e7af48cb70e1b33d23eeec67d92eb7
    Former-commit-id: 3260f3393d32ba2784febb4e233f74e2d7297254 [formerly 64493fb57e2d88e12876723502319c7abec7009e]
    Former-commit-id: 1256363f867fdfb91c6ba29a9fe0586a7a36d054

--- stock/serializers.py
+++ stock/serializers.py
@@ -1,30 +1,6 @@
 from rest_framework import serializers
 from .models import StockListModel, StockBinModel
-from userprofile.serializers import Users
-from rest_framework.exceptions import APIException
-import re
-
-def data_validate(data):
-    script_obj = re.findall(r'script', str(data), re.IGNORECASE)
-    select_obj = re.findall(r'select', str(data), re.IGNORECASE)
-    if script_obj:
-        raise APIException({'detail': 'Bad Data can‘not be store'})
-    elif select_obj:
-        raise APIException({'detail': 'Bad Data can‘not be store'})
-    else:
-        return data
-
-def openid_validate(data):
-    if Users.objects.filter(openid=data).exists():
-        return data
-    else:
-        raise APIException({'detail': 'User does not exists'})
-
-def appid_validate(data):
-    if Users.objects.filter(appid=data).exists():
-        return data
-    else:
-        raise APIException({'detail': 'User does not exists'})
+from utils import datasolve
 
 class StockListGetSerializer(serializers.ModelSerializer):
     goods_code = serializers.CharField(read_only=True, required=False)
@@ -48,8 +24,8 @@ class StockListGetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = StockListModel
-        exclude = ['openid', ]
-        read_only_fields = ['id', ]
+        exclude = ['openid']
+        read_only_fields = ['id']
 
 class StockBinGetSerializer(serializers.ModelSerializer):
     bin_name = serializers.CharField(read_only=True, required=False)
@@ -70,10 +46,10 @@ class StockBinGetSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'create_time', 'update_time', ]
 
 class StockBinPostSerializer(serializers.ModelSerializer):
-    openid = serializers.CharField(read_only=False, required=False, validators=[openid_validate])
-    bin_name = serializers.CharField(read_only=True, required=False, validators=[data_validate])
-    move_to_bin = serializers.CharField(read_only=True, required=False, validators=[data_validate])
-    move_qty = serializers.CharField(read_only=True, required=False, validators=[data_validate])
+    openid = serializers.CharField(read_only=False, required=False, validators=[datasolve.openid_validate])
+    bin_name = serializers.CharField(read_only=True, required=False, validators=[datasolve.data_validate])
+    move_to_bin = serializers.CharField(read_only=True, required=False, validators=[datasolve.data_validate])
+    move_qty = serializers.CharField(read_only=True, required=False, validators=[datasolve.data_validate])
 
     class Meta:
         model = StockBinModel
