424bb8ff2579e865634b307092517a1f1e63dd5d
Singosgu <Singosgu@gmail.com>
2021-04-30 15:38:15 +0800

    V 2.0.19

--- userlogin/apps.py
+++ userlogin/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class UserloginConfig(AppConfig):
     name = 'userlogin'
--- userregister/apps.py
+++ userregister/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class UserregisterConfig(AppConfig):
     name = 'userregister'
