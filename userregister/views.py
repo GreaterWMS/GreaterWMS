1e9415f90c32ab7c16afedbc64b568c819457411
Singosgu <Singosgu@gmail.com>
2021-04-30 15:17:42 +0800

    V 2.0.19

--- userregister/views.py
+++ userregister/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from userprofile.models import Users
-from userprofile import serializers
 from utils.fbmsg import FBMsg
 from utils.md5 import Md5
 from django.views.decorators.csrf import csrf_exempt
