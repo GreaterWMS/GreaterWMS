1c62308b685a7549c49f28a479e3e286ae4c46a1
Singosgu <Singosgu@gmail.com>
2021-05-10 12:26:40 +0800

    V 2.0.20
    Former-commit-id: 9dd167e7a1eeb40e19ac939d882c9463a6ef55ba [formerly ea0ef1984ae401673dc4714d35d58a5511f66f8f] [formerly ce508bc3b7c5b317a7af06e90408e4ea4c720624 [formerly 154136ca9c0057cb5edd29cf737cee95d2975b81 [formerly 154136ca9c0057cb5edd29cf737cee95d2975b81 [formerly 19f6c9e0208cf1a6066554f1e019a48f6d63d664]]]]
    Former-commit-id: e587479e42388b8da590f5b8f83df9f7ffd88729 [formerly 2b927cfcce659add8da8854e68ea0864968d1447]
    Former-commit-id: 41205bf16234595a1b52483322d1a4df43a49987
    Former-commit-id: 8a53a45193a0524978e9b5556e5385e13c655532
    Former-commit-id: d61ba1f4fb4a9cdcfafc59583253dbe4550f197a

---
+++ uploadfile/views.py
@@ -0,0 +1,45 @@
+from rest_framework import viewsets
+import pandas as pd
+import numpy as np
+from goods.models import ListModel as goodslist
+from . import serializers
+from utils.page import MyPageNumberPagination
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.exceptions import APIException
+from django.http import StreamingHttpResponse
+from rest_framework.settings import api_settings
+
+class GoodlistViewSet(viewsets.ModelViewSet):
+    """
+        create:
+            Create a data line（post）
+    """
+    queryset = goodslist.objects.all()
+    serializer_class = serializers.GoodslistSerializer
+    pagination_class = []
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return serializers.GoodslistSerializer
+        else:
+            return self.http_method_not_allowed(request=self.request)
+
+    def create(self, request, *args, **kwargs):
+        files = self.request.FILES.get('file')
+        if files:
+            excel_type = files.name.split('.')[1]
+            if excel_type in ['xlsx', 'xls', 'csv']:
+                df = pd.read_excel(files)
+                df.drop_duplicates(keep='first', inplace=True)
+                goods_list = df.drop_duplicates(subset=['商品单位'], keep='first')
+                # print(goods_list)
+                # for i in range(len(goods_list)):
+                #     print(i)
+                # print(df.drop_duplicates(subset=[a], keep='first'))
+            else:
+                raise APIException({"detail": "Can Not Support This File Type"})
+        else:
+            raise APIException({"detail": "Please Select One File"})
+        return Response({"detail": "Success"})
