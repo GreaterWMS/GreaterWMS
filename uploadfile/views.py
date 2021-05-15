cf35546fcacd9cfc6641a2e98c3a80c6ecb28fe7
Singosgu <Singosgu@gmail.com>
2021-05-15 12:08:58 +0800

    V 2.0.20

--- uploadfile/views.py
+++ uploadfile/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 import pandas as pd
 import numpy as np
 from utils.datasolve import data_validate
+from utils.datasolve import is_number
 from goods.models import ListModel as goodslist
 from goodsunit.models import ListModel as goodsunit
 from goodsclass.models import ListModel as goodsclass
@@ -64,73 +65,143 @@ class GoodlistfileViewSet(viewsets.ModelViewSet):
                 for d in range(len(data_list)):
                     data_validate(str(data_list[d]))
                 for i in range(len(data_list)):
-                    goodslist.objects.create(openid=self.request.auth.openid,
-                                             goods_code=data_list[i][0],
-                                             goods_desc=data_list[i][1],
-                                             goods_supplier=data_list[i][2],
-                                             goods_weight=data_list[i][3],
-                                             goods_w=data_list[i][4],
-                                             goods_d=data_list[i][5],
-                                             goods_h=data_list[i][6],
-                                             unit_volume=data_list[i][7],
-                                             goods_unit=data_list[i][8],
-                                             goods_class=data_list[i][9],
-                                             goods_brand=data_list[i][10],
-                                             goods_color=data_list[i][11],
-                                             goods_shape=data_list[i][12],
-                                             goods_specs=data_list[i][13],
-                                             goods_origin=data_list[i][14],
-                                             goods_cost=data_list[i][15],
-                                             goods_price=data_list[i][16],
-                                             creater=self.request.auth.name
-                                             )
+                    if str(data_list[i][0]) == 'nan':
+                        continue
+                    else:
+                        if str(data_list[i][1]) == 'nan':
+                            data_list[i][1] = 'None'
+                        if str(data_list[i][2]) == 'nan':
+                            data_list[i][2] = 'None'
+                        if is_number(str(data_list[i][3])):
+                            if str(data_list[i][3]) == 'nan':
+                                data_list[i][3] = 0
+                        else:
+                            data_list[i][3] = 0
+                        if is_number(str(data_list[i][4])):
+                            if str(data_list[i][4]) == 'nan':
+                                data_list[i][4] = 0
+                        else:
+                            data_list[i][4] = 0
+                        if is_number(str(data_list[i][5])):
+                            if str(data_list[i][5]) == 'nan':
+                                data_list[i][5] = 0
+                        else:
+                            data_list[i][5] = 0
+                        if is_number(str(data_list[i][6])):
+                            if str(data_list[i][6]) == 'nan':
+                                data_list[i][6] = 0
+                        else:
+                            data_list[i][6] = 0
+                        if is_number(str(data_list[i][7])):
+                            if str(data_list[i][7]) == 'nan':
+                                data_list[i][7] = 0
+                        else:
+                            data_list[i][7] = 0
+                        if str(data_list[i][8]) == 'nan':
+                            data_list[i][8] = 'None'
+                        if str(data_list[i][9]) == 'nan':
+                            data_list[i][9] = 'None'
+                        if str(data_list[i][10]) == 'nan':
+                            data_list[i][10] = 'None'
+                        if str(data_list[i][11]) == 'nan':
+                            data_list[i][11] = 'None'
+                        if str(data_list[i][12]) == 'nan':
+                            data_list[i][12] = 'None'
+                        if str(data_list[i][13]) == 'nan':
+                            data_list[i][13] = 'None'
+                        if str(data_list[i][14]) == 'nan':
+                            data_list[i][14] = 'None'
+                        if is_number(str(data_list[i][15])):
+                            if str(data_list[i][15]) == 'nan':
+                                data_list[i][15] = 0
+                        else:
+                            data_list[i][15] = 0
+                        if is_number(str(data_list[i][16])):
+                            if str(data_list[i][16]) == 'nan':
+                                data_list[i][16] = 0
+                        else:
+                            data_list[i][16] = 0
+                        goodslist.objects.create(openid=self.request.auth.openid,
+                                                 goods_code=str(data_list[i][0]).strip(),
+                                                 goods_desc=str(data_list[i][1]).strip(),
+                                                 goods_supplier=str(data_list[i][2]).strip(),
+                                                 goods_weight=data_list[i][3],
+                                                 goods_w=data_list[i][4],
+                                                 goods_d=data_list[i][5],
+                                                 goods_h=data_list[i][6],
+                                                 unit_volume=data_list[i][7],
+                                                 goods_unit=str(data_list[i][8]).strip(),
+                                                 goods_class=str(data_list[i][9]).strip(),
+                                                 goods_brand=str(data_list[i][10]).strip(),
+                                                 goods_color=str(data_list[i][11]).strip(),
+                                                 goods_shape=str(data_list[i][12]).strip(),
+                                                 goods_specs=str(data_list[i][13]).strip(),
+                                                 goods_origin=str(data_list[i][14]).strip(),
+                                                 goods_cost=data_list[i][15],
+                                                 goods_price=data_list[i][16],
+                                                 creater=self.request.auth.name
+                                                 )
                 goods_unit_list = df.drop_duplicates(subset=[data_header.get('goods_unit')], keep='first').loc[:,
                                     data_header.get('goods_unit')].values
-                for i in range(len(goods_unit_list)):
+                for i in goods_unit_list:
+                    if str(i) == 'nan':
+                        i = 'None'
                     goodsunit.objects.create(openid=self.request.auth.openid,
-                                             goods_unit=goods_unit_list[i],
+                                             goods_unit=str(i).strip(),
                                              creater=self.request.auth.name
                                              )
                 goods_class_list = df.drop_duplicates(subset=[data_header.get('goods_class')], keep='first').loc[:,
                                     data_header.get('goods_class')].values
-                for i in range(len(goods_class_list)):
+                for i in goods_class_list:
+                    if str(i) == 'nan':
+                        i = 'None'
                     goodsclass.objects.create(openid=self.request.auth.openid,
-                                              goods_class=goods_class_list[i],
+                                              goods_class=str(i).strip(),
                                               creater=self.request.auth.name
                                               )
                 goods_brand_list = df.drop_duplicates(subset=[data_header.get('goods_brand')], keep='first').loc[:,
                                     data_header.get('goods_brand')].values
-                for i in range(len(goods_brand_list)):
+                for i in goods_brand_list:
+                    if str(i) == 'nan':
+                        i = 'None'
                     goodsbrand.objects.create(openid=self.request.auth.openid,
-                                              goods_brand=goods_brand_list[i],
+                                              goods_brand=str(i).strip(),
                                               creater=self.request.auth.name
                                               )
                 goods_color_list = df.drop_duplicates(subset=[data_header.get('goods_color')], keep='first').loc[:,
                                     data_header.get('goods_color')].values
-                for i in range(len(goods_color_list)):
+                for i in goods_color_list:
+                    if str(i) == 'nan':
+                        i = 'None'
                     goodscolor.objects.create(openid=self.request.auth.openid,
-                                              goods_color=goods_color_list[i],
+                                              goods_color=str(i).strip(),
                                               creater=self.request.auth.name
                                               )
                 goods_shape_list = df.drop_duplicates(subset=[data_header.get('goods_shape')], keep='first').loc[:,
                                     data_header.get('goods_shape')].values
-                for i in range(len(goods_shape_list)):
+                for i in goods_shape_list:
+                    if str(i) == 'nan':
+                        i = 'None'
                     goodsshape.objects.create(openid=self.request.auth.openid,
-                                              goods_shape=goods_shape_list[i],
+                                              goods_shape=str(i).strip(),
                                               creater=self.request.auth.name
                                               )
                 goods_specs_list = df.drop_duplicates(subset=[data_header.get('goods_specs')], keep='first').loc[:,
                                     data_header.get('goods_specs')].values
-                for i in range(len(goods_specs_list)):
+                for i in goods_specs_list:
+                    if str(i) == 'nan':
+                        i = 'None'
                     goodsspecs.objects.create(openid=self.request.auth.openid,
-                                              goods_specs=goods_specs_list[i],
+                                              goods_specs=str(i).strip(),
                                               creater=self.request.auth.name
                                               )
                 goods_origin_list = df.drop_duplicates(subset=[data_header.get('goods_origin')], keep='first').loc[:,
                                     data_header.get('goods_origin')].values
-                for i in range(len(goods_origin_list)):
+                for i in goods_origin_list:
+                    if str(i) == 'nan':
+                        i = 'None'
                     goodsorigin.objects.create(openid=self.request.auth.openid,
-                                               goods_origin=goods_origin_list[i],
+                                               goods_origin=str(i).strip(),
                                                creater=self.request.auth.name
                                                )
             else:
@@ -178,15 +249,34 @@ class SupplierfileViewSet(viewsets.ModelViewSet):
                 for d in range(len(data_list)):
                     data_validate(str(data_list[d]))
                 for i in range(len(data_list)):
-                    supplier.objects.create(openid=self.request.auth.openid,
-                                            supplier_name=data_list[i][0],
-                                            supplier_city=data_list[i][1],
-                                            supplier_address=data_list[i][2],
-                                            supplier_contact=data_list[i][3],
-                                            supplier_manager=data_list[i][4],
-                                            supplier_level=data_list[i][5],
-                                            creater=self.request.auth.name
-                                            )
+                    if str(data_list[i][0]) == 'nan':
+                        continue
+                    else:
+                        if str(data_list[i][1]) == 'nan':
+                            data_list[i][1] = 'None'
+                        if str(data_list[i][2]) == 'nan':
+                            data_list[i][2] = 'None'
+                        if is_number(str(data_list[i][3])):
+                            if str(data_list[i][3]) == 'nan':
+                                data_list[i][3] = 0
+                        else:
+                            data_list[i][3] = 0
+                        if str(data_list[i][4]) == 'nan':
+                            data_list[i][4] = 'None'
+                        if is_number(str(data_list[i][5])):
+                            if str(data_list[i][5]) == 'nan':
+                                data_list[i][5] = 0
+                        else:
+                            data_list[i][5] = 0
+                        supplier.objects.create(openid=self.request.auth.openid,
+                                                supplier_name=str(data_list[i][0]).strip(),
+                                                supplier_city=str(data_list[i][1]).strip(),
+                                                supplier_address=str(data_list[i][2]).strip(),
+                                                supplier_contact=data_list[i][3],
+                                                supplier_manager=str(data_list[i][4]).strip(),
+                                                supplier_level=data_list[i][5],
+                                                creater=self.request.auth.name
+                                                )
             else:
                 raise APIException({"detail": "Can Not Support This File Type"})
         else:
@@ -233,15 +323,34 @@ class CustomerfileViewSet(viewsets.ModelViewSet):
                 for d in range(len(data_list)):
                     data_validate(str(data_list[d]))
                 for i in range(len(data_list)):
-                    customer.objects.create(openid=self.request.auth.openid,
-                                            customer_name=data_list[i][0],
-                                            customer_city=data_list[i][1],
-                                            customer_address=data_list[i][2],
-                                            customer_contact=data_list[i][3],
-                                            customer_manager=data_list[i][4],
-                                            customer_level=data_list[i][5],
-                                            creater=self.request.auth.name
-                                            )
+                    if str(data_list[i][0]) == 'nan':
+                        continue
+                    else:
+                        if str(data_list[i][1]) == 'nan':
+                            data_list[i][1] = 'None'
+                        if str(data_list[i][2]) == 'nan':
+                            data_list[i][2] = 'None'
+                        if is_number(str(data_list[i][3])):
+                            if str(data_list[i][3]) == 'nan':
+                                data_list[i][3] = 0
+                        else:
+                            data_list[i][3] = 0
+                        if str(data_list[i][4]) == 'nan':
+                            data_list[i][4] = 'None'
+                        if is_number(str(data_list[i][5])):
+                            if str(data_list[i][5]) == 'nan':
+                                data_list[i][5] = 0
+                        else:
+                            data_list[i][5] = 0
+                        customer.objects.create(openid=self.request.auth.openid,
+                                                customer_name=str(data_list[i][0]).strip(),
+                                                customer_city=str(data_list[i][1]).strip(),
+                                                customer_address=str(data_list[i][2]).strip(),
+                                                customer_contact=data_list[i][3],
+                                                customer_manager=str(data_list[i][4]).strip(),
+                                                customer_level=data_list[i][5],
+                                                creater=self.request.auth.name
+                                                )
             else:
                 raise APIException({"detail": "Can Not Support This File Type"})
         else:
