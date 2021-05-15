13714099e3012da3529ea3cc81596ab03933174f
Singosgu <Singosgu@gmail.com>
2021-05-15 10:08:02 +0800

    V 2.0.20
    Former-commit-id: f19cc8ecee6c720ea6e7297e025dfca30017f77d [formerly d62a8483b46f3efde2798d833053ba1725c11645] [formerly f1263ee152ed05decb37b81a5146bddd578587a6 [formerly 5b16f37edd915d75504a45749839402d07197b01 [formerly 5b16f37edd915d75504a45749839402d07197b01 [formerly 94d49c9653cb90466e35f3f506284f4a3818461a]]]]
    Former-commit-id: d9849505191bfdaf909232bafcceb1ff25f47fcd [formerly e1d9de837f02997b042ca56e2da23cb7002e8cf7]
    Former-commit-id: 4af3505f7c3fdffb84b3885e053a1d99e1a7cf88
    Former-commit-id: 7ae99c3b674e5a179cc753aca9851cfecb2b280b
    Former-commit-id: 77323bb92567dcac1b4f1d97387196c1d62ff541

--- utils/auth.py
+++ utils/auth.py
@@ -5,15 +5,18 @@ class Authtication(object):
     def authenticate(self, request):
         if request.path == '/docs/':
             return (False, None)
-        elif request.path == '/swagger':
+        elif request.path == '/swagger/':
                 return (False, None)
         else:
             openid = request.META.get('HTTP_TOKEN')
             if openid:
-                user = Users.objects.filter(openid__exact=str(openid)).first()
-                return (True, user)
+                if Users.objects.filter(openid__exact=str(openid)).exists():
+                    user = Users.objects.filter(openid__exact=str(openid)).first()
+                    return (True, user)
+                else:
+                    raise APIException({"detail": "User Does Not Exists"})
             else:
-                raise APIException({"detail": "Please Add token to your request headers"})
+                raise APIException({"detail": "Please Add Openid To Your Request Headers"})
 
     def authenticate_header(self, request):
         pass
