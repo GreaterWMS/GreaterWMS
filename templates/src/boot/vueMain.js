9ae424c536222509e3d19a4f5f42b0afa28be84d
Singosgu <Singosgu@gmail.com>
2021-05-22 05:42:39 +0800

    V 2.0.20

--- templates/src/boot/vueMain.js
+++ templates/src/boot/vueMain.js
@@ -4,4 +4,9 @@ import VDistpicker from 'v-distpicker'
 export default async ({ app, router, store, Vue }) => {
   Vue.component('v-distpicker', VDistpicker)
   Vue.use(Print)
+  console.log('Welcome To GreaterWMS')
+  console.log('Home Page ------ https://www.56yhz.com/')
+  console.log('Demo Page ------ https://wms.56yhz.com/')
+  console.log('Gitee Page ------ https://gitee.com/Singosgu/GreaterWMS')
+  console.log('GitHub Page ------ https://github.com/Singosgu/GreaterWMS')
 }
