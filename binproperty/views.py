1a81bc102f59d953615bcd62746fd3fc7dad7a88
Singosgu <Singosgu@gmail.com>
2021-05-31 08:12:50 +0800

    V 2.0.21
    Former-commit-id: 335c24fcf07393b20077a3e0c8c4bd052f547014 [formerly 7782d92666e3652d0a5a5596e587ae01eff49e67] [formerly 29f7aa2160119f3b4d0d448f049760035c3d4b8e [formerly 7ca59feb82795f042ddf2823b1a37fec89fda008 [formerly 7ca59feb82795f042ddf2823b1a37fec89fda008 [formerly 407e01751e8e6f71163edcab0bdfdc4ddeb762ca]]]]
    Former-commit-id: d5c20548c3e06069cfb454eafba96b699b9335ff [formerly 34f8686c6af7bc961eb47549a163106afcbae3ca]
    Former-commit-id: 555d50833b3fe419d30e0aa6ee3a43d26abdd168
    Former-commit-id: 56cf21e764a4bd8b8bb69bb726b08fb5c7803115
    Former-commit-id: 74be8338640fca27c14693d3f84f28b3319cf4a9

--- binproperty/views.py
+++ binproperty/views.py
@@ -4,23 +4,24 @@ from . import serializers
 from utils.page import MyPageNumberPagination
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from .filter import Filter
-from rest_framework.exceptions import APIException
-from django.db.models import Q
-
 
 class APIViewSet(viewsets.ModelViewSet):
     """
         list:
             Response a data list（all）
     """
-    queryset = ListModel.objects.all()
     serializer_class = serializers.BinpropertyGetSerializer
     pagination_class = MyPageNumberPagination
     filter_backends = [DjangoFilterBackend, OrderingFilter, ]
     ordering_fields = ['id', "create_time", "update_time", ]
     filter_class = Filter
+
+    def get_queryset(self):
+        if self.request.user:
+            return ListModel.objects.filter(is_delete=False)
+        else:
+            return ListModel.objects.none()
 
     def get_serializer_class(self):
         if self.action == 'list':
