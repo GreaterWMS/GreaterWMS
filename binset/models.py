635cfc7af706903f50a2e6e9ac36b0670d7722a7
Singosgu <Singosgu@gmail.com>
2021-06-01 08:21:22 +0800

    V 2.0.21

--- binset/models.py
+++ binset/models.py
@@ -6,6 +6,7 @@ class ListModel(models.Model):
     bin_property = models.CharField(max_length=11, verbose_name="Bin Property")
     empty_label = models.BooleanField(default=True, verbose_name="Empty Label")
     creater = models.CharField(max_length=255, verbose_name="Who Created")
+    bar_code = models.CharField(max_length=255, verbose_name="Bar Code")
     openid = models.CharField(max_length=255, verbose_name="Openid")
     is_delete = models.BooleanField(default=False, verbose_name='Delete Label')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="Create Time")
