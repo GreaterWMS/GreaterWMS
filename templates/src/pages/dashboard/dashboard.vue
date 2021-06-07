a2336419035b06d7575d86ec62a71f8a1ba83afd
Singosgu <Singosgu@gmail.com>
2021-06-07 09:38:19 +0800

    V 2.0.21
    Former-commit-id: 5756da6b38f2482851a301192c8a731b3c6be62a [formerly f5f0fb0db3ad8b4632bd20d458638c33a4be76bc] [formerly 0ea8936410475483d5080e1f4b8202019e8f2ffc [formerly c2411af227d81d5e63a58b9cd4abf5377e1b89e7 [formerly c2411af227d81d5e63a58b9cd4abf5377e1b89e7 [formerly 6c0b29974aee74c4be947d82e59bc1cd7516b961]]]]
    Former-commit-id: 4521a71dc47ff144e533a691701c4c4750d8e694 [formerly 74131e5a30087950f62b98234e72ef45eee919cb]
    Former-commit-id: 446025e0017e543e58372ce59912fea157eca19e
    Former-commit-id: 9ed0e07ede99e9b270f6f4fd2fe907cbb69a9f62
    Former-commit-id: 1d5d344505dc5702caf98b906a6792e90d035c71

---
+++ templates/src/pages/dashboard/dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-top">
+    <div class="main-table">
+      <router-view />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Pagedashboard',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
