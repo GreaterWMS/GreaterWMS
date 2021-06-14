e6835594f083d813484ab712e5d0bf44d300f06d
Singosgu <Singosgu@gmail.com>
2021-06-14 10:16:18 +0800

    V 2.0.21
    Former-commit-id: e3107aca12916f006a8a03907cba3378eb672a8e [formerly 15a73d91d3f745aadd07653e66c54d1b6ef03d19] [formerly b8fe11e20dfd1f4569932ac95ec8e6acaa73aa13 [formerly 938a66b3e4ea86d7f9bf42bb6d08943936860017 [formerly e6808f41d34e52754010985c5fe14cf8cd5a0546] [formerly 566ebf3c9343acfaf2623569f5357845d6976300] [formerly 566ebf3c9343acfaf2623569f5357845d6976300 [formerly c14ab897092279295cc0dabda45204cacce58a6a]]]]
    Former-commit-id: 2e8815a722dea25efacb434edde851c38ab72728 [formerly fe8aa0df754e95328aca1eaef6c290f2e7aa28c6]
    Former-commit-id: 986510a9b9b8c81854a86211055d70af578b94a1
    Former-commit-id: de07eb4f5a3af983c9b4fa816ec8e457907afc4e
    Former-commit-id: b7be94e9306a7ea903d826ee15f005655e4f1d8d [formerly d1c30e80d6831896b68597f0f8a2ddfd4925ac5e]
    Former-commit-id: 0175e8384bd3dfbcac861d59c1c7eed0600d6ff5

--- greaterwms/views.py
+++ greaterwms/views.py
@@ -3,42 +3,42 @@ from django.conf import settings
 from wsgiref.util import FileWrapper
 import mimetypes
 
-async def RobotsRe(request):
+async def robots(request):
     path = settings.BASE_DIR + request.path_info
     content_type, encoding = mimetypes.guess_type(path)
     resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
     resp['Cache-Control'] = "max-age=864000000000"
     return resp
 
-async def FaviconRe(request):
+async def favicon(request):
     path = str(settings.BASE_DIR) + '/static/img/logo.png'
     content_type, encoding = mimetypes.guess_type(path)
     resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
     resp['Cache-Control'] = "max-age=864000000000"
     return resp
 
-async def CssRe(request):
+async def css(request):
     path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
     content_type, encoding = mimetypes.guess_type(path)
     resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
     resp['Cache-Control'] = "max-age=864000000000"
     return resp
 
-async def JsRe(request):
+async def js(request):
     path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
     content_type, encoding = mimetypes.guess_type(path)
     resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
     resp['Cache-Control'] = "max-age=864000000000"
     return resp
 
-async def StaticsRe(request):
+async def statics(request):
     path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
     content_type, encoding = mimetypes.guess_type(path)
     resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
     resp['Cache-Control'] = "max-age=864000000000"
     return resp
 
-def FontsRe(request):
+def fonts(request):
     path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
     content_type, encoding = mimetypes.guess_type(path)
     resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
