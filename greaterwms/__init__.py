e226e17611fa341c406826f6e7b9cf8dc474555e
Singosgu <Singosgu@gmail.com>
2021-07-07 10:12:05 +0800

    V 2.0.22

--- greaterwms/__init__.py
+++ greaterwms/__init__.py
@@ -9,7 +9,7 @@ mimetypes.add_type("text/javascript", ".js", True)
 try:
     path = os.path.join(settings.BASE_DIR, 'utils/authorization.txt')
     if os.path.exists(path) is False:
-        response = requests.post('https://wms.56yhz.com/area_v2/')
+        response = requests.post('https://production.56yhz.com/area_v2/')
         with open(path, 'w') as f:
             f.write(str(eval(response.text).get('check_token')))
         f.close()
