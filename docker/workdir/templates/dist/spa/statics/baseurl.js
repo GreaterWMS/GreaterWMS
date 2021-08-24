8cd81780013421bdc790683c7131d8d0426235b8
Singosgu <Singosgu@gmail.com>
2021-08-24 17:12:33 +0800

    delete

--- docker/workdir/templates/dist/spa/statics/baseurl.js
+++
@@ -1,8 +0,0 @@
-const protocol = location.protocol;
-
-const host = window.location.host;
-
-const baseurl = `${protocol}//${host}/`,
-  wsurl = `${protocol === "https" ? "wss:" : "ws:"}//${host}/`;
-
-window.g = { BaseUrl: baseurl, WsUrl: wsurl };
--- docker_start.sh
+++
@@ -1,3 +0,0 @@
-##构建业务镜像
-docker build -t cow11023:GreaterWMSv2.0.24 .
-docker-compose up -d
