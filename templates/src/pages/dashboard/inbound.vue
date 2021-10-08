78612575523fcb119fa7dee69784d78ed109fc5a
Singosgu <Singosgu@gmail.com>
2021-10-08 11:49:05 +0800

    V 2.0.26

--- templates/src/pages/dashboard/inbound.vue
+++ templates/src/pages/dashboard/inbound.vue
@@ -1,5 +1,6 @@
 <template>
-<q-card class="bg-white q-ml-sm shadow-11">
+  <q-page class="q-pa-md bg-grey-2 ">
+    <q-card class="bg-white q-ml-sm shadow-11">
   <q-card-section>
     <div class="text-h6 text-grey-8 text-weight-bolder">
       Bar Chart
@@ -8,12 +9,12 @@
                 :options="product_options" label="Select Product"/>
     </div>
   </q-card-section>
-  <q-card-section class="q-pa-none">
-    <IEcharts :option="barChartOption" :resizable="true" style="height: 600px; width: 100%"/>
+  <q-card-section class="q-pa-none map_height">
+    <IEcharts :option="getBarChartOptions" :resizable="true" style="width: 100%; height: 600px"/>
   </q-card-section>
 </q-card>
+  </q-page>
 </template>
-
 
 <script>
 import IEcharts from 'vue-echarts-v3/src/full.js'
@@ -22,30 +23,49 @@ export default {
   name: 'charts',
   data () {
     return {
-      barChartOption: {
+      selected_product: 'Matcha Latte',
+      data: [
+        { product: 'Matcha Latte', 2015: 43.3, 2016: 85.8, 2017: 93.7 },
+        { product: 'Milk Tea', 2015: 83.1, 2016: 73.4, 2017: 55.1 },
+        { product: 'Cheese Cocoa', 2015: 86.4, 2016: 65.2, 2017: 82.5 },
+        { product: 'Walnut Brownie', 2015: 72.4, 2016: 53.9, 2017: 39.1 }
+      ],
+      product_options: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie']
+    }
+  },
+  computed: {
+    getBarChartOptions () {
+      var _this = this
+
+      // eslint-disable-next-line camelcase
+      var filtered_data = _this.data.filter(function (item) {
+        return item.product === _this.selected_product
+      })
+
+      let buisness
+      return {
+
         grid: {
           bottom: '25%'
         },
-        legend: {},
+        xAxis: {
+          type: 'category',
+          axisLabel: {},
+          nameLocation: 'middle',
+          nameGap: 78
+        },
         tooltip: {},
         dataset: {
           dimensions: ['product', '2015', '2016', '2017'],
-          source: [
-            { product: 'Matcha Latte', 2015: 43.3, 2016: 85.8, 2017: 93.7 },
-            { product: 'Milk Tea', 2015: 83.1, 2016: 73.4, 2017: 55.1 },
-            { product: 'Cheese Cocoa', 2015: 86.4, 2016: 65.2, 2017: 82.5 },
-            { product: 'Walnut Brownie', 2015: 72.4, 2016: 53.9, 2017: 39.1 }
-          ]
+          source: filtered_data
         },
-        xAxis: {
-          type: 'category',
-          axisLabel: {
-            rotate: 45
+        buisness,
+        yAxis: {
+          type: 'value',
+          splitLine: {
+            show: false
           }
         },
-        yAxis: {},
-        // Declare several bar series, each will be mapped
-        // to a column of dataset.source by default.
         series: [
           { type: 'bar' },
           { type: 'bar' },
