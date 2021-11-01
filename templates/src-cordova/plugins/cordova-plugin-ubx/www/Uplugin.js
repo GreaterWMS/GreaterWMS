45219c7d308c2dfb8d94c3c0d4946babadbbd8c9
Singosgu <Singosgu@gmail.com>
2021-11-01 13:34:00 +0800

    V 2.1.0

--- templates/src-cordova/plugins/cordova-plugin-ubx/www/Uplugin.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.getDeviceID = function (arg0, success, error) {
-    exec(success, error, 'Uplugin', 'getDeviceID', [arg0]);
-};
-
-exports.getBarcode = function (arg0, success, error) {
-    exec(success, error, 'Uplugin', 'getBarcode', [arg0]);
-};
