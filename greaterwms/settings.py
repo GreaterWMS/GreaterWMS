bbe1b804b00c41109554b8287f366786614be037
Elvis.Shi <Singosgu@gmail.com>
2021-12-04 12:51:05 +0800

    V 2.1.0

--- greaterwms/settings.py
+++ greaterwms/settings.py
@@ -110,6 +110,7 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
