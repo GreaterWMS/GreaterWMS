00c6697fa47ced090263d9f0c33568fafea57ca5
‘ChangxiaoChen’ <329025421@qq.com>
2021-12-24 19:54:09 +0800

    update

--- goods/urls.py
+++ goods/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
 path(r'', views.APIViewSet.as_view({"get": "list", "post": "create"}), name="goods"),
 path(r'file/', views.FileDownloadView.as_view({"get": "list"}), name="goodslistfiledownload"),
-re_path(r'^(?P<pk>\d+)/$', views.APIViewSet.as_view({
+re_path(r'^(?P<pk>\d+)/', views.APIViewSet.as_view({
     'get': 'retrieve',
     'put': 'update',
     'patch': 'partial_update',
     'delete': 'destroy'
-}), name="goods_1")
+}), name="goods_1"),
+    path(r'goodstag/<str:bar_code>/',views.SannerGoodsTagView.as_view({"get":"retrieve"}))
 ]
--- scanner/urls.py
+++ scanner/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 path(r'', views.ListViewSet.as_view({"get": "list"}), name="scanner"),
+path(r'list/<str:bar_code>/', views.SannerView.as_view({"get": "retrieve"})),
+path(r'sanerpicking/' , views.SannerDnDetailPickingListView.as_view({"get":"list"}),name="sanerpicking"),
 ]
