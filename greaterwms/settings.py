756d0c10635799198659c940642c393639c41b3a
Dacheng Chen <chen@maritimedatasystems.com>
2022-01-06 12:18:15 +0100

    add postgres support

--- greaterwms/settings.py
+++ greaterwms/settings.py
@@ -103,10 +103,21 @@ WSGI_APPLICATION = 'greaterwms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 # update
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wms',
+        'USER': 'docker',
+        'PASSWORD': 'docker',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
