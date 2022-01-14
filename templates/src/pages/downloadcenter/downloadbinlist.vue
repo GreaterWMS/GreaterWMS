0413ec48c36549462af47882155363e5ca1b5bee
caiwei-0627 <68604067+caiwei-0627@users.noreply.github.com>
2022-01-14 11:58:45 +0800

    download_center bug fix

--- templates/src/pages/downloadcenter/downloadbinlist.vue
+++ templates/src/pages/downloadcenter/downloadbinlist.vue
@@ -2,7 +2,7 @@
   <div>
     <transition appear enter-active-class="animated fadeIn">
       <q-table
-        class=" shadow-24"
+        class="shadow-24"
         :data="table_list"
         row-key="id"
         :separator="separator"
@@ -18,49 +18,124 @@
       >
         <template v-slot:top>
           <div class="flex items-center">
-            <div class="q-mr-md">{{ $t('download_center.createTime') }}</div>
-            <q-input readonly outlined dense v-model="createDate2" :placeholder="interval">
+            <div class="q-mr-md">{{ $t("download_center.createTime") }}</div>
+            <q-input
+              readonly
+              outlined
+              dense
+              v-model="createDate2"
+              :placeholder="interval"
+            >
               <template v-slot:append>
                 <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale"><q-date v-model="createDate1" range /></q-popup-proxy>
+                  <q-popup-proxy
+                    ref="qDateProxy"
+                    transition-show="scale"
+                    transition-hide="scale"
+                    ><q-date v-model="createDate1" range
+                  /></q-popup-proxy>
                 </q-icon>
               </template>
             </q-input>
             <q-btn-group push class="q-ml-md">
-              <q-btn :label="$t('download_center.reset')" icon="img:statics/downloadcenter/reset.svg" @click="getList()">
-                <q-tooltip content-class="bg-amber text-black shadow-4" :offset="[10, 10]" content-style="font-size: 12px">{{ $t('download_center.reset') }}</q-tooltip>
+              <q-btn
+                :label="$t('download_center.reset')"
+                icon="img:statics/downloadcenter/reset.svg"
+                @click="reset()"
+              >
+                <q-tooltip
+                  content-class="bg-amber text-black shadow-4"
+                  :offset="[10, 10]"
+                  content-style="font-size: 12px"
+                  >{{ $t("download_center.reset") }}</q-tooltip
+                >
               </q-btn>
-              <q-btn :label="$t('downloadasnlist')" icon="cloud_download" @click="downloadlistData()">
-                <q-tooltip content-class="bg-amber text-black shadow-4" :offset="[10, 10]" content-style="font-size: 12px">{{ $t('downloadasnlisttip') }}</q-tooltip>
+              <q-btn
+                :label="$t('downloadasnlist')"
+                icon="cloud_download"
+                @click="downloadlistData()"
+              >
+                <q-tooltip
+                  content-class="bg-amber text-black shadow-4"
+                  :offset="[10, 10]"
+                  content-style="font-size: 12px"
+                  >{{ $t("downloadasnlisttip") }}</q-tooltip
+                >
               </q-btn>
             </q-btn-group>
           </div>
         </template>
         <template v-slot:body="props">
           <q-tr :props="props">
-           <q-td key="bin_name" :props="props">{{ props.row.bin_name }}</q-td>
-           <q-td key="goods_code" :props="props">{{ props.row.goods_code }}</q-td>
-           <q-td key="goods_desc" :props="props">{{ props.row.goods_desc }}</q-td>
-           <q-td key="goods_qty" :props="props">{{ props.row.goods_qty }}</q-td>
-           <q-td key="pick_qty" :props="props">{{ props.row.pick_qty }}</q-td>
-           <q-td key="picked_qty" :props="props">{{ props.row.picked_qty }}</q-td>
-           <q-td key="bin_size" :props="props">{{ props.row.bin_size }}</q-td>
-           <q-td key="bin_property" :props="props">{{ props.row.bin_property }}</q-td>
-           <q-td key="create_time" :props="props">{{ props.row.create_time }}</q-td>
-           <q-td key="update_time" :props="props">{{ props.row.update_time }}</q-td>
+            <q-td key="bin_name" :props="props">{{ props.row.bin_name }}</q-td>
+            <q-td key="goods_code" :props="props">{{
+              props.row.goods_code
+            }}</q-td>
+            <q-td key="goods_desc" :props="props">{{
+              props.row.goods_desc
+            }}</q-td>
+            <q-td key="goods_qty" :props="props">{{
+              props.row.goods_qty
+            }}</q-td>
+            <q-td key="pick_qty" :props="props">{{ props.row.pick_qty }}</q-td>
+            <q-td key="picked_qty" :props="props">{{
+              props.row.picked_qty
+            }}</q-td>
+            <q-td key="bin_size" :props="props">{{ props.row.bin_size }}</q-td>
+            <q-td key="bin_property" :props="props">{{
+              props.row.bin_property
+            }}</q-td>
+            <q-td key="create_time" :props="props">{{
+              props.row.create_time
+            }}</q-td>
+            <q-td key="update_time" :props="props">{{
+              props.row.update_time
+            }}</q-td>
           </q-tr>
         </template>
       </q-table>
     </transition>
     <template>
       <div class="q-pa-lg flex flex-center">
-        <q-btn v-show="pathname_previous" flat push color="purple" :label="$t('previous')" icon="navigate_before" @click="getListPrevious()">
-          <q-tooltip content-class="bg-amber text-black shadow-4" :offset="[10, 10]" content-style="font-size: 12px">{{ $t('previous') }}</q-tooltip>
+        <q-btn
+          v-show="pathname_previous"
+          flat
+          push
+          color="purple"
+          :label="$t('previous')"
+          icon="navigate_before"
+          @click="getListPrevious()"
+        >
+          <q-tooltip
+            content-class="bg-amber text-black shadow-4"
+            :offset="[10, 10]"
+            content-style="font-size: 12px"
+            >{{ $t("previous") }}</q-tooltip
+          >
         </q-btn>
-        <q-btn v-show="pathname_next" flat push color="purple" :label="$t('next')" icon-right="navigate_next" @click="getListNext()">
-          <q-tooltip content-class="bg-amber text-black shadow-4" :offset="[10, 10]" content-style="font-size: 12px">{{ $t('next') }}</q-tooltip>
+        <q-btn
+          v-show="pathname_next"
+          flat
+          push
+          color="purple"
+          :label="$t('next')"
+          icon-right="navigate_next"
+          @click="getListNext()"
+        >
+          <q-tooltip
+            content-class="bg-amber text-black shadow-4"
+            :offset="[10, 10]"
+            content-style="font-size: 12px"
+            >{{ $t("next") }}</q-tooltip
+          >
         </q-btn>
-        <q-btn v-show="!pathname_previous && !pathname_next" flat push color="dark" :label="$t('no_data')"></q-btn>
+        <q-btn
+          v-show="!pathname_previous && !pathname_next"
+          flat
+          push
+          color="dark"
+          :label="$t('no_data')"
+        ></q-btn>
       </div>
     </template>
   </div>
@@ -103,7 +178,8 @@ export default {
       createDate1: '',
       createDate2: '',
       date_range: '',
-      url: ''
+      searchUrl: '',
+      downloadUrl:'stock/filebinlist/'
     };
   },
   computed: {
@@ -117,11 +193,13 @@ export default {
         if (val.to) {
           this.createDate2 = `${val.from} - ${val.to}`;
           this.date_range = `${val.from},${val.to} 23:59:59`;
-          this.url = this.pathname + 'bin/?' + 'create_time__range=' + this.date_range
+          this.searchUrl = this.pathname + 'bin/?' + 'create_time__range=' + this.date_range
+          this.downloadhUrl = this.pathname + 'filebinlist/?' + 'create_time__range=' + this.date_range
         } else {
           this.createDate2 = `${val}`;
           this.dateArray = val.split('/');
-          this.url = this.pathname + 'bin/?' + 'create_time__year=' + this.dateArray[0] + '&' + 'create_time__month=' + this.dateArray[1] + '&' + 'create_time__day=' + this.dateArray[2];
+          this.searchUrl = this.pathname + 'bin/?' + 'create_time__year=' + this.dateArray[0] + '&' + 'create_time__month=' + this.dateArray[1] + '&' + 'create_time__day=' + this.dateArray[2];
+           this.downloadhUrl = this.pathname + 'filebinlist/?' + 'create_time__year=' + this.dateArray[0] + '&' + 'create_time__month=' + this.dateArray[1] + '&' + 'create_time__day=' + this.dateArray[2];
         }
         this.date_range = this.date_range.replace(/\//g, '-');
         this.getSearchList();
@@ -148,7 +226,7 @@ export default {
     },
     getSearchList() {
       var _this = this;
-      getauth(_this.url)
+      getauth(_this.searchUrl)
         .then(res => {
           _this.table_list = res.results;
           _this.pathname_previous = res.previous;
@@ -196,7 +274,7 @@ export default {
     },
     downloadlistData() {
       var _this = this;
-      getfile(_this.pathname + 'filebinlist/?lang=' + LocalStorage.getItem('lang')).then(res => {
+      getfile(_this.downloadhUrl).then(res => {
         var timeStamp = Date.now();
         var formattedString = date.formatDate(timeStamp, 'YYYYMMDDHHmmssSSS');
         const status = exportFile(_this.pathname + 'list' + formattedString + '.csv', '\uFEFF' + res.data, 'text/csv');
@@ -209,6 +287,11 @@ export default {
         }
       });
     },
+    reset(){
+        this.getList()
+        this.downloadUrl = 'stock/filebinlist/'
+        this.createDate2 = '';
+    }
   },
   created() {
     var _this = this
