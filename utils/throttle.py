09aa0f62f8b7dcbbb6ea1e83a79022ef1f9dd42f
Elvis.Shi <Singosgu@gmail.com>
2022-02-04 13:01:08 +0800

    V 2.1.1

--- utils/throttle.py
+++ utils/throttle.py
@@ -10,7 +10,7 @@ class VisitThrottle(BaseThrottle):
         if request.path == '/docs/':
             return (False, None)
         elif request.path == '/swagger/':
-                return (False, None)
+            return (False, None)
         else:
             ip = request.META.get('HTTP_X_FORWARDED_FOR') if request.META.get(
                 'HTTP_X_FORWARDED_FOR') else request.META.get('REMOTE_ADDR')
