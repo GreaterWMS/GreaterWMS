398085a8c01df98c7a92bb8cf200b07fa1559668
Elvis.Shi <Singosgu@gmail.com>
2022-04-22 21:41:38 +0800

    V 2.1.6

--- templates/public/statics/baseurl.js
+++ templates/public/statics/baseurl.js
@@ -1,5 +1,5 @@
-const baseurl = 'https://production.56yhz.com/'
-const wsurl = 'wss://production/websocket/'
+const baseurl = 'http://127.0.0.1:8008/'
+const wsurl = 'ws://127.0.0.1:8008/'
 
 window.g = {
   BaseUrl: baseurl,
