7d8f141e05f2e323ed28ac9618ab87f795a0da43
Elvis.Shi <Singosgu@gmail.com>
2022-12-02 19:07:12 +0800

    V 2.1.26

--- greaterwms/asgi.py
+++ greaterwms/asgi.py
@@ -1,23 +1,15 @@
-"""
-ASGI config for IM56 project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 from utils.websocket import websocket_application
+from asgihandler.core import ASGIHandler
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greaterwms.settings')
 
 http_application = get_asgi_application()
 
 async def application(scope, receive, send):
     if scope['type'] in ['http', 'https']:
-        await http_application(scope, receive, send)
+        ASGIHandler.asgi_get_handler(scope)
     elif scope['type'] in ['websocket']:
         await websocket_application(scope, receive, send)
     else:
