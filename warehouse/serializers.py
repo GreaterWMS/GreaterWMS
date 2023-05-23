d998f47d28b6681d86e9640ac7e942c8bed80b0c
Elvis.Shi <Singosgu@gmail.com>
2023-05-24 06:03:31 +0800

    V 2.1.27

--- warehouse/serializers.py
+++ warehouse/serializers.py
@@ -10,12 +10,13 @@ class WarehouseGetSerializer(serializers.ModelSerializer):
     warehouse_contact = serializers.CharField(read_only=True, required=False)
     warehouse_manager = serializers.CharField(read_only=True, required=False)
     creater = serializers.CharField(read_only=True, required=False)
+    openid = serializers.CharField(read_only=True, required=False)
     create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
     update_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M:%S')
 
     class Meta:
         model = ListModel
-        exclude = ['openid', 'is_delete', ]
+        exclude = ['is_delete', ]
         read_only_fields = ['id', ]
 
 
--- warehouse/urls.py
+++ warehouse/urls.py
@@ -8,5 +8,9 @@ re_path(r'^(?P<pk>\d+)/$', views.APIViewSet.as_view({
     'put': 'update',
     'patch': 'partial_update',
     'delete': 'destroy'
+}), name="warehouse_1"),
+path(r'multiple/', views.MultipleViewSet.as_view({"get": "list"}), name="warehouse"),
+re_path(r'^multiple/(?P<pk>\d+)/$', views.MultipleViewSet.as_view({
+    'get': 'retrieve',
 }), name="warehouse_1")
 ]
