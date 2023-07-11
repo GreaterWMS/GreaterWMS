172af3d4b7193b74e920f744f48be169dcd3924d
Elvis.Shi <Singosgu@gmail.com>
2023-07-12 05:45:03 +0800

    V2.1.41

--- greaterwms/settings.py
+++ greaterwms/settings.py
@@ -105,6 +105,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,
+        }
     }
 }
 
