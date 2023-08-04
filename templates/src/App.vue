b5620fad0ff712da87b62aa81aad0cbe8685a8b8
Elvis.Shi <Singosgu@gmail.com>
2023-08-04 15:38:44 +0800

    V2.1.47

--- templates/src/App.vue
+++ templates/src/App.vue
@@ -4,11 +4,6 @@
   </div>
 </template>
 <script >
-import Vconsole from "vconsole";
-if (process.env.NODE_ENV !== 'production') {
-  const vConsole = new Vconsole()
-}
-
 export default {
   name: 'App'
 }
