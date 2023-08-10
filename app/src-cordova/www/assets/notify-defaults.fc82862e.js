fd4767404dd4e3ff6ea60f6bf2258cd1bdf4ccd9
Elvis.Shi <Singosgu@gmail.com>
2023-08-10 14:49:06 +0800

    V2.1.47

---
+++ app/src-cordova/www/assets/notify-defaults.fc82862e.js
@@ -0,0 +1,1 @@
+import{N as t}from"./index.1785b037.js";t.setDefaults({position:"top",type:"positive",progress:!0,timeout:1500,textColor:"white",classes:"push"});
---
+++ app/src-cordova/www/assets/use-quasar.8d75fc37.js
@@ -0,0 +1,1 @@
+import{i as a,ax as r}from"./index.1785b037.js";function u(){return a(r)}export{u};
