5a247b53fb9322b01fdbc9487d2f81c4ba172c00
Elvis.Shi <Singosgu@gmail.com>
2023-12-27 18:26:24 +0800

    V2.1.48

--- throttle/models.py
+++ throttle/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class ListModel(models.Model):
     openid = models.CharField(max_length=255, verbose_name="Openid")
     appid = models.CharField(max_length=255, verbose_name="Appid")
-    ip = models.CharField(max_length=32, verbose_name="IP")
+    ip = models.CharField(max_length=255, verbose_name="IP")
     method = models.CharField(max_length=18, verbose_name="Method")
     t_code = models.CharField(max_length=255, verbose_name="Transaction Code")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="Create Time")
