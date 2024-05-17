4aaec92337a30a05e2de32a68abd0d9b33bcae51
Wahyu Surya Dharma <wahyusuryadharmaa@gmail.com>
2024-05-17 03:10:47 +0000

    add new db pointing

--- greaterwms/settings.py
+++ greaterwms/settings.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(verbose=True)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
