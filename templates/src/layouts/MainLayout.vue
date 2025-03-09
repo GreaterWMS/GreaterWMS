dd9fc0e4973b77805c7f32cac89dc3026df16a09
Tony Jiang <yujiang99@gmail.com>
2025-03-08 17:57:54 -0800

    Upgrade compiler version

--- templates/src/layouts/MainLayout.vue
+++ templates/src/layouts/MainLayout.vue
@@ -95,23 +95,6 @@
         </transition>
         <transition appear enter-active-class="animated zoomIn">
           <q-btn
-            icon="img:statics/icons/GitHub.png"
-            round
-            dense
-            flat
-            @click="brownlink('https://github.com/GreaterWMS/GreaterWMS')"
-            style="margin: 0 10px 0 10px"
-          >
-            <q-tooltip
-              content-class="bg-amber text-black shadow-4"
-              :offset="[15, 15]"
-              content-style="font-size: 12px"
-              >GitHub Link</q-tooltip
-            >
-          </q-btn>
-        </transition>
-        <transition appear enter-active-class="animated zoomIn">
-          <q-btn
             icon="api"
             round
             dense
